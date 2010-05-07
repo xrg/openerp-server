5cb7edb354e3c3450a1306f45c7a2fbba9fcee94
P. Christeas <p_christ@hol.gr>
2010-05-07 12:33:11 +0300

    Update copyright of my code, convert to LGPLv2
    I allow my code to be reused in other projects, without silly
    restrictions. At the same time I explicitly *do not* allow anybody
    to put restrictions on this code (ie. GPLv3 and/or Affero).

--- bin/service/websrv_lib.py
+++ bin/service/websrv_lib.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright P. Christeas <p_christ@hol.gr> 2008,2009
+# Copyright P. Christeas <p_christ@hol.gr> 2008-2010
 # A part of the code comes from the ganeti project:  http://www.mail-archive.com/ganeti-devel@googlegroups.com/msg00713.html#
 #
 # WARNING: This program as such is intended to be used by professional
@@ -11,14 +11,13 @@
 # Service Company
 #
 # This program is Free Software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; version 2 of the License.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
