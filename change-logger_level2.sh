b5fb2889401aeae1b18234be3c02c3c6ed350c3f
P. Christeas <p_christ@hol.gr>
2010-06-22 20:59:49 +0300

    Scripts: change-logger-level for new RPC api.

---
+++ change-logger_level2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ADMIN_PASSWD='admin'
+method_1() {
+	cat '-' << EOF
+<xml>
+<methodCall>
+	<methodName>set_loglevel</methodName>
+	<params>
+		<param><value><string>$ADMIN_PASSWD</string></value>
+		</param>
+		<param>
+		<value><string>$2</string></value>
+		<value><string>$1</string></value>
+		</param>
+	</params>
+</methodCall>
+EOF
+}
+LEVEL=10
+
+if [ -z "$1" ] ; then
+	echo "Usage: $0 <logger> [<level>]"
+	echo
+	echo "Where <logger> is the specific one, like 'osv', 'db.connection' etc."
+	echo "      and <level> a numeric val, 10= debug "
+	echo
+	exit 1
+fi
+
+if [ -n "$2" ] ; then LEVEL=$2 ; fi
+
+method_1 $1 $LEVEL | POST -c 'text/xml' http://localhost:8069/xmlrpc/common
+#eof
