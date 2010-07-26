e39c5066a972d591431ad12ad970e7c40e579c48
P. Christeas <p_christ@hol.gr>
2010-07-26 12:33:34 +0300

    HTTP lib: When authentication cannot proceed, issue 403
    The http protocol states that 401 should only be used when further
    authentication would be possible

--- bin/service/websrv_lib.py
+++ bin/service/websrv_lib.py
@@ -17,7 +17,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
+# GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
@@ -30,17 +30,9 @@
 
 import socket
 import base64
-import errno
-import os
 import SocketServer
 from BaseHTTPServer import *
 from SimpleHTTPServer import SimpleHTTPRequestHandler
-
-try:
-    from ssl import SSLError
-except ImportError:
-    class SSLError(socket.error):
-        pass
 
 class AuthRequiredExc(Exception):
     def __init__(self,atype,realm):
