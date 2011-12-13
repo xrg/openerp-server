397e52dc5ec5c379adbedc7a9f7c6f5a109a8547
P. Christeas <xrg@linux.gr>
2011-12-13 20:53:23 +0200

    orm: get_last_modified() call for whole model
    Unlike perm_read() which retrieves the modification time for a model,
    this returns the last modification for the full dataset, or partial slice
    (with domain expression).
    
    It used to be in caldav, but designed to eventually come here.

--- bin/osv/orm.py
+++ bin/osv/orm.py
@@ -4959,6 +4959,30 @@ class orm(orm_template):
                 results[k] = ''
         return results
 
+    def get_last_modified(self, cr, user, args, context=None, access_rights_uid=None):
+        """Return the last modification date of objects in 'domain'
+        This function has similar semantics to orm.search(), apart from the
+        limit, offset and order arguments, which make no sense here.
+        It is useful when we want to find if the table (aka set of records)
+        has any modifications we should update at the client.
+        """
+        if context is None:
+            context = {}
+        self.pool.get('ir.model.access').check(cr, access_rights_uid or user, self._name, 'read', context=context)
+
+        query = self._where_calc(cr, user, args, context=context)
+        self._apply_ir_rules(cr, user, query, 'read', context=context)
+        from_clause, where_clause, where_clause_params = query.get_sql()
+
+        where_str = where_clause and (" WHERE %s" % where_clause) or ''
+
+        cr.execute('SELECT MAX(COALESCE("%s".write_date, "%s".create_date)) FROM ' % (self._table, self._table) + 
+                    from_clause + where_str ,
+                    where_clause_params,
+                    debug=self._debug)
+        res = cr.fetchall()
+        return res[0][0]
+
 class orm_deprecated(object):
     """ Mix-in for deprecated models.
 
