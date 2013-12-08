cfd6c13545321c4910a63c6a94ba9ffb0e99f706
P. Christeas <xrg@linux.gr>
2013-12-08 09:22:17 +0200

    fields.function: use symbol_set from shadow field
    Rather than a "switch" on the field type, copy the setter functions
    of the shadow field, symmetrically.

--- bin/osv/fields_function.py
+++ bin/osv/fields_function.py
@@ -335,20 +335,9 @@ class function(_column):
             if type=='binary':
                 self._symbol_get=lambda x:x and str(x)
 
-        if type == 'float':
-            self._symbol_c = float._symbol_c
-            self._symbol_f = float._symbol_f
-            self._symbol_set = float._symbol_set
-
-        if type == 'boolean':
-            self._symbol_c = boolean._symbol_c
-            self._symbol_f = boolean._symbol_f
-            self._symbol_set = boolean._symbol_set
-
-        if type in ['integer','integer_big']:
-            self._symbol_c = integer._symbol_c
-            self._symbol_f = integer._symbol_f
-            self._symbol_set = integer._symbol_set
+        self._symbol_c = self._shadow._symbol_c
+        self._symbol_f = self._shadow._symbol_f
+        self._symbol_set = self._shadow._symbol_set
 
     def digits_change(self, cr):
         self._shadow.digits_change(cr)
